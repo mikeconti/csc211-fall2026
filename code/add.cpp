e050416c6721a89230f1cc5c7ef79a7497131fa7
Michael Conti <michaelconti@uri.edu>
2026-09-17 16:17:59 -0400

    update

---
+++ code/add.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(){
+
+    int a;
+    int b;
+
+
+    std::cin >> a; // 6
+    std::cin >> b; // 4
+
+    std::cout << a << " + " << b << " = " << a +  b << std::endl;
+
+    //Desired output:
+        // a + b = <a> + <b>
+
+
+
+    return 0;
+}
